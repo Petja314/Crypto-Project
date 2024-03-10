5068b990653a3f8e81f871c4939c2c21ae4afef5
dmitrijs <petja314@gmail.com>
2024-03-10 18:03:20 +0000

    Portfolio Component - portfolio table calculation v2

--- src/commons/formattedPrice.ts
+++ src/commons/formattedPrice.ts
@@ -1,5 +1,18 @@
 //Reusable Function to format the price in decimals
 
-export const formattedPrice = (price: any) => {
-    return Number(price).toLocaleString('en-US')
-}
+// export const formattedPrice = (price: any) => {
+//     return Number(price).toLocaleString('en-US')
+// }
+
+
+export const formattedPrice = (price: any, decimalPrecision: number = 2) => {
+    const numericPrice = Number(price);
+
+    const formattedNumber = new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: decimalPrecision,
+        maximumFractionDigits: decimalPrecision,
+    }).format(numericPrice);
+
+    return formattedNumber;
+};
+
